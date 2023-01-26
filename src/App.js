06b1ea1d912040a80a963806b9337ce22182e08a
Muhammad Shehroze <111072848+shehroze321@users.noreply.github.com>
2023-01-26 12:22:42 +0500

    React live weather app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+
+import './App.css';
+import TempApp from './components/TempApp';
+
+function App() {
+  return (
+    <>
+      <TempApp/>
+    </>
+  );
+}
+
+export default App;
